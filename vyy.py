e29e64c9dcfe87b07ffe5d04c9414d4b66913cbe
vytoriaoliveira <vytoria.santosfo@gmail.com>
2026-06-16 12:59:22 -0300

    Add files via upload

---
+++ vyy.py
@@ -0,0 +1,8 @@
+nota1 = int(input("digite a nota do 1°Bimestre: "))
+nota2 = int(input("digete a nota do 2°Bimestre: "))
+nota3 = int(input("digite a nota do 3°Bimestre : "))
+nota4 = int(input("digite a nota do 4°Bimestre: " ))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+print("sua nota é: ", media)
