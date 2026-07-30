ce0664d42a01ac2792cf1f36f8fcfbd1865ec6ba
vytoriaoliveira <vytoria.santosfo@gmail.com>
2026-07-30 13:05:44 -0300

    Add files via upload

---
+++ vy.py
@@ -0,0 +1,13 @@
+n1 = float(input("Digite um numero: "))
+n2 = float(input("Digite outro numero: "))
+
+soma = n1 + n2 
+subtracao = n1 - n2
+multiplicacao = n1 * n2 
+divisao = n1/n2
+
+print("A soma do numero ", n1," e ",n2," é ", soma)
+print("A subtração do numero", n1," e ", n2," é ", subtracao) 
+print("A multiplicação do numero ", n1," e ",n2," é ", multiplicacao)
+print("A divisão do numero ", n1," e ", n2," é ", divisao)
+
