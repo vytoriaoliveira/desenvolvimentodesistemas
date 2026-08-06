94def9e82322eb14ec351da4519954025d26d5a5
vytoriaoliveira <vytoria.santosfo@gmail.com>
2026-08-06 12:13:29 -0300

    Add files via upload

---
+++ cachorro.py
@@ -0,0 +1,8 @@
+nota1 = float (input("coloque sua nota: "))
+nota2 = float (input("coloque sua nota: "))
+nota3 = float (input("coloque sua nota: "))
+nota4 = float (input("coloque sua nota: "))
+
+media = (nota1+nota2+nota3+nota4)/4
+
+print("sua media foi:", media)
