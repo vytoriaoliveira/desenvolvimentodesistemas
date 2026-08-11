cee27976bb6e5e008e0c55a1d4ea0aa0695e31b6
vytoriaoliveira <vytoria.santosfo@gmail.com>
2026-08-11 13:03:55 -0300

    Add files via upload

---
+++ regra3.py
@@ -0,0 +1,7 @@
+massa = float(input("digite um numero: "))
+gravidade = float(input("digite um numero: "))
+altura = float(input("digite um numero: "))
+
+energia = massa * gravidade * altura 
+
+print("o total: ", energia)
