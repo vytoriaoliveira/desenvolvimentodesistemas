1b7ccc24e4bdc227c85d1c0af085f7979d0ccc6c
vytoriaoliveira <vytoria.santosfo@gmail.com>
2026-08-25 12:59:39 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,8 @@
+nota1 = float(input("digite um numero: "))
+nota2 = float(input("digite o numero: "))
+nota3 = float(input("digite o numero: "))
+nota4 = float(input("digite o numero: "))
+
+media = (nota1 * nota2 * nota3 * nota4)/4
+
+print("digite o valor total:", media)
