ec7818e71357185454b61d4144cd6b178e6547be
vytoriaoliveira <vytoria.santosfo@gmail.com>
2026-09-22 13:00:51 -0300

    Add files via upload

---
+++ numero2.py
@@ -0,0 +1,8 @@
+numero = float(input("digite o numero: "))
+
+if numero>0:
+    print("positivo")
+elif numero==0:
+    print ("zero")
+else:
+    print("negativo")
---
+++ temperaturav.py
@@ -0,0 +1,11 @@
+temperatura = float(input("digite a teperatura:"))
+
+if temperatura>=35:
+    print("muito quente")
+elif temperatura>=30:
+    print ("quente")
+elif temperatura>=18:
+    print ("fresco")
+else:
+    print("frio")
+    
