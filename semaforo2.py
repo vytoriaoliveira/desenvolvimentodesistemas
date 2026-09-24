cf7b2c4c94eddd058634792133ef4bc2ba91a0aa
vytoriaoliveira <vytoria.santosfo@gmail.com>
2026-09-24 12:41:21 -0300

    Add files via upload

---
+++ semaforo2.py
@@ -0,0 +1,8 @@
+cor_do_semaforo = input("qual a cor do semaforo? " )
+if cor_do_semaforo =="verde":
+    print("siga em frente")
+elif cor_do_semaforo =="amarelo":
+    print ("atenção")
+else:
+    print("espere o sinal ficar verde")
+    
